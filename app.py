9a4efa427b7fc2d5dda2fe2452cd8804cf0fddbb
chloe020915-boop <chloe020915@gmail.com>
2025-08-24 16:00:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("你是谁？")
 user_name = st.text_input("请输入你的名字")
 if user_name:
-    if user_name == "曾灏":
-        st.success("原来你是曾灏!")
+    if user_name == "范铱伦":
+        st.success("原来你是范铱伦!")
     else:
-        st.success("你为什么不是曾灏？")
+        st.success("你为什么不是范铱伦？")
